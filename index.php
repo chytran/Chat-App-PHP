7e6f34f5981a0334f93e23c80d2383fa294a325e
chytran <chytran@users.noreply.github.com>
2021-08-20 12:02:20 -0700

    Adding Jquery

--- index.php
+++ index.php
@@ -5,10 +5,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chat App PHP</title>
-
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+        
+    </script>
     <!--====== CSS ======-->
+    <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>">
 </head>
 <body>
-    
+    <div id="messages"></div>
+    <form action="" type="text">
+        <input type="text" id="message" autocomplete="off" autofocus placeholder="Type Message...">
+        <input type="submit" value="Send">
+    </form>
 </body>
 </html>
