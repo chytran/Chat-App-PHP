a078874f501ef8ce77d06512d87ba8190640ad50
chytran <chytran@users.noreply.github.com>
2021-08-21 12:51:25 -0700

    database set up

--- includes/database.inc.php
+++ includes/database.inc.php
@@ -10,4 +10,18 @@ if($db->connect_error) {
     die("Connection failed: " . $db->connect_error);
 }
 
+$result = array();
+$message = isset($_POST['message']) ? $_POST['message'] : null;
+$from = isset($_POST['from']) ? $_POST["from"] : null;
+
+if (!empty($messagge) && !empty($from)) {
+    $sql = "INSERT INTO `chat` (`message`, `from`) VALUES ('".$message."', '".$from."')";
+    $result['send_status'] = $db->query($sql);
+}
+
 $db->close();
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+echo json_encode($result);
